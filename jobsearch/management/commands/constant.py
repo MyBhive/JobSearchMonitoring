91f4ef9c4540641e9907dec770f275b4745ff257
MyBhive <delphine.miel0318@gmail.com>
2021-09-05 17:29:04 +0300

    constant to fill in the DB

---
+++ jobsearch/management/commands/constant.py
@@ -0,0 +1,26 @@
+﻿# coding: utf-8
+
+STATUS = [
+    "cv-envoye",
+    "a-postuler",
+    "reponse-positive",
+    "reponse-negative",
+    "entretien"
+]
+
+TYPE_OF_CONTRACT = [
+    "CDI",
+    "CDD",
+    "stage",
+    "interim",
+    "freelance",
+    "extra",
+    "autres"
+]
+
+STYLE_OF_CONTRACT = [
+    "temps-plein",
+    "temps-partiel",
+    "extra",
+    "autres"
+]
