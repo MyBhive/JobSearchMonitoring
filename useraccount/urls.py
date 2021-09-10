43848cdd67b2b778dfbeb6e343e22de81d165a39
MyBhive <delphine.miel0318@gmail.com>
2021-09-10 16:59:21 +0300

    reset password pathS

--- useraccount/urls.py
+++ useraccount/urls.py
@@ -1,6 +1,9 @@
 ﻿# coding: utf-8
 from django.urls import path
-
+from django.contrib.auth.views import PasswordResetView, \
+    PasswordResetDoneView, \
+    PasswordResetConfirmView, \
+    PasswordResetCompleteView
 from . import views
 
 """Path to connect views to the frontend"""
@@ -9,5 +12,29 @@ urlpatterns = [
     path('login/', views.log_in, name="login"),
     path('sign_in/', views.sign_in, name='sign_in'),
     path('log_out/', views.log_out, name='log_out'),
+
+    path('password_reset/',
+         PasswordResetView.as_view(
+             template_name='userpages/reset_password.html'
+         ),
+         name='password_reset'),
+
+    path('password_reset/done/',
+         PasswordResetDoneView.as_view(
+             template_name='userpages/password_reset_done.html'
+         ),
+         name='password_reset_done'),
+
+    path('password_reset_confirm/<uidb64>/<token>/',
+         PasswordResetConfirmView.as_view(
+             template_name='userpages/password_reset_confirm.html'
+         ),
+         name='password_reset_confirm'),
+
+    path('password_reset/complete/',
+         PasswordResetCompleteView.as_view(
+             template_name='userpages/password_reset_complete.html'
+         ),
+         name='password_reset_complete'),
     path('mon_compte/', views.my_account, name='my_account'),
 ]
