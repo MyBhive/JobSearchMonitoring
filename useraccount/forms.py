7c45bb0d6e7e702ddcd28d64dfdb5fbf40a6d9c0
MyBhive <delphine.miel0318@gmail.com>
2021-09-10 16:58:38 +0300

    forms to use email and not username

--- useraccount/forms.py
+++ useraccount/forms.py
@@ -1,22 +1,21 @@
-from django import forms
-from .models import CustomUser
+
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
 
 
-class SignInForm(UserCreationForm):
-    """Create form for sign_in"""
-    surname = forms.CharField()
-    name = forms.CharField()
-    email = forms.EmailField()
-    date_of_birth = forms.DateField()
+class UserAdminCreationForm(UserCreationForm):
+    """
+    A Custom form for creating new users.
+    """
 
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = [
-            'surname',
-            'name',
+            'first_name',
+            'last_name',
             'email',
             'password1',
             'password2',
             'date_of_birth'
         ]
+
