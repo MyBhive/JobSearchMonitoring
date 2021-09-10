280e6ed80896e08dac86f27567e0520feddf7497
MyBhive <delphine.miel0318@gmail.com>
2021-09-10 17:00:58 +0300

    add email backend file

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -30,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
-
+EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
+EMAIL_FILE_PATH = os.path.join(BASE_DIR.joinpath('sent_emails'))
 # Application definition
 
 INSTALLED_APPS = [
