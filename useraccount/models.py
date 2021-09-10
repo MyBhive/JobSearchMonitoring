0b0781ec42b236a4d09ed4e496dbcdc753fb3015
MyBhive <delphine.miel0318@gmail.com>
2021-09-10 11:49:40 +0300

    change customuser pattern

--- useraccount/models.py
+++ useraccount/models.py
@@ -5,12 +5,10 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     """This model override the 'django user model' that we can work on it"""
-    surname = models.CharField(max_length=100, null=False, blank=False)
-    name = models.CharField(max_length=100, null=False, blank=False)
-    email = models.EmailField(max_length=50, null=False, blank=False)
-    password1 = models.CharField(max_length=15, null=False, blank=False)
-    password2 = models.CharField(max_length=15, null=False, blank=False)
+    surname = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
     date_of_birth = models.DateField(default=None, null=True)
+
 
     def __str__(self):
         return self.surname
