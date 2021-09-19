16bb89333cb9401f1bb4ab69a8befa2d03e81e7d
MyBhive <delphine.miel0318@gmail.com>
2021-09-19 16:02:20 +0300

    path to categories

--- jobsearch/urls.py
+++ jobsearch/urls.py
@@ -5,6 +5,12 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('mentions_legales', views.legal_notices, name='legal_notices'),
+    path('legal_notices', views.legal_notices, name='legal_notices'),
     path('contact', views.contact, name='contact'),
+    path('see_categories', views.see_categories, name='see_categories'),
+    path('add_categories', views.add_category, name='add_categories'),
+    path('enter_category/<str:id_cat>/', views.enter_category, name='enter_category'),
+    path('delete_category/<str:cat_id>', views.delete_category, name='delete_category'),
+    path('add_job_offer', views.add_job_offer, name='add_job_offer'),
+    path('job_description', views.see_job_offer, name='job_description'),
 ]
