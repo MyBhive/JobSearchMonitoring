e5e10d3ac5d988fe93aecd713d37f70c549460a4
MyBhive <delphine.miel0318@gmail.com>
2021-09-26 15:57:04 +0300

    createview

---
+++ jobsearch/forms.py
@@ -0,0 +1,12 @@
+﻿from django.views.generic import CreateView
+from jobsearch.models import *
+from django import forms
+
+
+class CreateJobOffer(CreateView):
+    model = JobOffer
+
+    template_name = 'pages/add_job.html'
+    fields = '__all__'
+
+
