7893cb280ac87f5384e5ae29933e131a0dc83b36
MyBhive <delphine.miel0318@gmail.com>
2021-10-17 20:07:09 +0300

    changing the DB structure

--- jobsearch/models.py
+++ jobsearch/models.py
@@ -17,18 +17,6 @@ class Categories(models.Model):
         return self.name_category
 
 
-class InfoUpload(models.Model):
-    """
-    Class to create a table to upload
-    cv and motivation letter depending of the job offer saved
-    """
-    motiv_letter = models.BinaryField(null=True, blank=True)
-    cv = models.BinaryField(null=True, blank=True)
-
-    def __str__(self):
-        return self.cv
-
-
 class Status(models.Model):
     """
     Class to create a table to save the status of a research
@@ -79,9 +67,11 @@ class JobOffer(models.Model):
     title = models.CharField(max_length=100)
     company_name = models.CharField(max_length=150, null=False)
     url = models.URLField(max_length=500, null=False, blank=False)
-    date = models.DateField(null=False, blank=False)
+    date = models.DateField(auto_now_add=True)
     salary = models.DecimalField(max_digits=19, decimal_places=3, null=False, blank=False)
     comments = models.TextField(max_length=800, null=True, blank=True)
+    motiv_letter = models.FileField(null=True, blank=True, upload_to='files/')
+    cv = models.FileField(null=True, blank=True, upload_to='files/')
     category_id = models.ForeignKey(
         Categories, on_delete=models.CASCADE
     )
@@ -97,12 +87,9 @@ class JobOffer(models.Model):
     type_id = models.ForeignKey(
         TypeOfContract, on_delete=models.CASCADE
     )
-    info_id = models.ForeignKey(
-        InfoUpload, on_delete=models.CASCADE, null=True
-    )
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
-        return reverse('job_detail', args=(str(self.pk)))
+        return reverse('job_detail',  kwargs={"pk": self.pk})
